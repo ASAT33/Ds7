16c603f95e20ab141ee03b43d9d141e8a3e2af5b
asat33 <143765544+ASAT33@users.noreply.github.com>
2023-09-05 09:56:17 -0500

    Add files via upload

---
+++ Lab13.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.3</title>
+</head>
+<center>
+<body>
+    <?php
+ echo phpinfo();
+    ?>
+</center>
+</body>
+</html>
