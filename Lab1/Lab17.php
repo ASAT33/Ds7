b8c3b86ec7eea810bde83ffe46d76469e5978f80
asat33 <143765544+ASAT33@users.noreply.github.com>
2023-09-05 15:19:58 -0500

    Add files via upload

---
+++ Lab1/Lab17.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Laboratorio 1.7</title>
+</head>
+<body>
+    <?php
+    $x =24;
+    $pi = 3.1416;
+    $animal = "conejo";
+    $saludo ="Hola caracola";
+    echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo;
+  
+?>
+</body>
+</html>
