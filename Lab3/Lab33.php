c06a88b23327ee557b638488322be053b66c9344
asat33 <abidielsoto@gmail.com>
2023-09-06 18:57:08 -0500

    Update Lab33.php

--- Lab3/Lab33.php
+++ Lab3/Lab33.php
@@ -1,42 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laboratorio 3.3</title>
+     <title>Laboratorio 3.3</title>
 </head>
 <body>
-    <?php
-    if ($_REQUEST['Apellido'] !=""){
-    {
-        echo "El apellido Ingresado es: $_REQUEST[Apellido]";
-    }
-    else {
-        echo "Favor coloque el apellido";
-    }
-    echo "<BR>";
-    if ($_REQUEST['Nombre']!="")
-    {
-        echo "El nombre Ingresado es: $_REQUEST[Nombre]";
-    }
-    else
-    {
-        echo "Favor coloque el nombre";
-
-    }
-}
-
-else{
-?>
-<FORM ACTION = "Lab33.php" METHOD ="POST">
-    Nombre: <INPUT TYPE = "TEXT" NAME="Nombre"><br>
-    Apellido: <INPUT TYPE ="TEXT" NAME="Apellido"><br>
-    <INPUT TYPE ="SUBMIT" NAME="enviar" VALUE="Enviar datos">
-</FORM>
-
 <?php
+    if (array_key_exists('enviar', $_POST)) {
+        if($_REQUEST['Apellido'] != "" )
+        {
+            echo "El apellido es: " . $_REQUEST['Apellido'];
+        }
+        else
+        {
+            echo "Favor coloque el apellido";
+        }
+        echo "<br>";
 
-
+        if ($_REQUEST['Nombre'] != "") 
+        {
+            echo "El nombre Ingresado es: " . $_REQUEST['Nombre'];
+        }
+        else
+        {
+            echo "Favor coloque el nombre";
+        }
+}
+else {
+ ?>
+ <Form action = "lab33.php" method = "POST">
+Nombre: <input type = "text" name = "Nombre"><br>
+Apellido: <input type = "text" name = "Apellido"><br>
+<input type = "submit" name = "enviar" value = "Enviar datos">
+</Form>
+<?php
 }
 ?>
 </body>
