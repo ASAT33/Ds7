1686076083706be65e1efac172ba98215daf1f09
asat33 <abidielsoto@gmail.com>
2023-10-18 21:29:19 -0500

    s

--- proyfinal3/Proyecto2/cerrar_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Limpiar cualquier sesión existente
-session_start();
-session_destroy();
-// Redirigir de regreso a la página de inicio de sesión
-header("Location: index.html");
-exit();
-?>
