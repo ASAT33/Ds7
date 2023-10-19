c0e8c4f79bc8d5b96b5260684dcbe8fa38b55767
asat33 <abidielsoto@gmail.com>
2023-10-18 21:41:14 -0500

    nuevo

--- Proyecto 1/navbar.php
+++ Proyecto 1/navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="lista_tareas.php">Lista de Tareas</a></li>
+        <li><a href="index.php">Lista de Tareas</a></li>
         <li><a href="agregar_tarea.php">Agregar Tarea</a></li>
         <li><a href="todas_las_tareas.php">Todas las Tareas</a></li>
     </ul>
